710573f9f1fb862777e6746437def106a4fa6a2b
Tatianna Robinson <19850813+Tatiannna@users.noreply.github.com>
2018-11-07 17:56:45 -0500

    remove alt.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
 router.use('/auth', require('./auth'));
 router.use('/users', require('./users'));
