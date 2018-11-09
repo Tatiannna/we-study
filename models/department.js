a43cb57a3518341b1b8187e6003a90f6c589ab5a
michelleli723 <michelleli94237@gmail.com>
2018-11-08 20:20:12 -0500

    add department model

---
+++ models/department.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Department = sequelize.define('department', {
+        id: {
+            type: DataTypes.INTEGER,
+            unique: true,
+        },
+        name: {
+            type: sequelize.ENUM,
+            values: ['CSCI', 'ENG', 'CHEM', 'BIOL', 'MATH', 'CHIN']
+        },
+    });
+
+    Department.associate = (models) => {
+        models.Department.hasMany(models.Course);
+    }
+
+    return Department;
+}
