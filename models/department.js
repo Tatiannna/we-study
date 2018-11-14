6e3e7d781c9814dee68ea6cef16c27eae5171032
michelleli723 <michelleli94237@gmail.com>
2018-11-13 21:11:33 -0500

    add local changes

--- models/department.js
+++ models/department.js
@@ -1,10 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     const Department = sequelize.define('department', {
-        id: {
-            primaryKey: true,
-            type: DataTypes.INTEGER,
-            unique: true,
-        },
+        // id: {
+        //     type: DataTypes.INTEGER,
+        //     unique: true,
+        // },
         name: {
             type: DataTypes.ENUM,
             values: ['CSCI', 'ENG', 'CHEM', 'BIOL', 'MATH', 'CHIN']
@@ -12,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     Department.associate = (models) => {
-        models.Department.hasMany(models.Course, {foreignKey: 'id', sourceKey: 'id'});
+        models.Department.hasMany(models.Course);
     }
 
     return Department;
